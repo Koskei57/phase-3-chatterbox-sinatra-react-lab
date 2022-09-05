5f898cd925338a0925384fa8137723cebe298c79
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-09-05 14:43:04 +0300

    Done

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,35 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
+  set :default_content_type, "application/json"
+
   # Add routes
-  
+
+  get "/messages" do
+    messages = Message.all.order(created_at: :ASC)
+    messages.to_json
+  end
+
+  #post
+  post "/messages" do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username],
+    )
+    message.to_json
+    #update
+  end
+
+  patch "/messages/:id" do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body],
+    )
+    message.to_json
+  end
+  #delete
+
+  delete "/messages/:id" do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
 end
